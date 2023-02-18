6fa79dd0d8533201bf9d9ed0703bb95beec339c6
Damcass <110790740+Damcass@users.noreply.github.com>
2023-02-17 20:23:08 -0400

    Add files via upload

---
+++ TipoDeVariaveis.java
@@ -0,0 +1,22 @@
+package variaveis;
+
+public class TipoDeVariaveis {
+
+	public static void main(String[] args) {
+		// Tipos de variaveis
+
+		int idade;
+		idade = 25;
+		int livros;
+		livros = 7;
+		int cadernos;
+		cadernos = 2 + 3;
+		int canetas;
+		canetas = 5;
+
+		System.out.print(livros + canetas);
+		System.out.print("O resultado é:" +idade);
+
+	}
+
+}
